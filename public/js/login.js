c545a049bd6960a50dfbe55a09be2af9940d9864
juvigh <juliolk.4321@gmail.com>
2022-11-11 15:52:41 -0300

    alteracoes finais

--- public/js/login.js
+++ public/js/login.js
@@ -6,39 +6,39 @@ const token = localStorage.getItem("token");
 const tokenObject = JSON.parse(token);
 console.log(tokenObject);
 
-fetch("https://debugrace-backend.onrender.com/status", {
-  method: "POST",
-  headers: { "Content-type": "application/json" },
-  body: JSON.stringify({ email: usuarioObject.email }),
-})
-  .then((res) => res.json())
-  .then((res) => {
-    const logado = res.statusLogado;
-    const status = logado.statusLogin;
-    console.log(status);
-    if (!status || !tokenObject) {
-      window.location.assign("https://debugrace-30568.web.app/login");
-    } else {
-      console.log(logado);
-      const configElement = document.querySelector("#item1");
-      const logoutButton = document.querySelector("#item2");
+// fetch("https://debugrace-backend.onrender.com/status", {
+//   method: "POST",
+//   headers: { "Content-type": "application/json" },
+//   body: JSON.stringify({ email: usuarioObject.email }),
+// })
+//   .then((res) => res.json())
+//   .then((res) => {
+//     const logado = res.statusLogado;
+//     const status = logado.statusLogin;
+//     console.log(status);
+//     if (!status || !tokenObject) {
+//       window.location.assign("https://debugrace-30568.web.app/login");
+//     } else {
+//       console.log(logado);
+//       const configElement = document.querySelector("#item1");
+//       const logoutButton = document.querySelector("#item2");
 
-      configElement.innerHTML = "CONFIGURAÇÕES";
-      configElement.href = "/configuracoes";
-      logoutButton.innerHTML = "SAIR";
-      logoutButton.removeAttribute("href");
-      logoutButton.addEventListener("click", () => {
-        localStorage.removeItem("token");
-        fetch("https://debugrace-backend.onrender.com/deslog", {
-          method: "POST",
-          headers: { "Content-type": "application/json" },
-          body: JSON.stringify({ email: usuarioObject.email }),
-        }).then((res) => res.json());
-        localStorage.removeItem("usuario");
-        window.location.assign("https://debugrace-30568.web.app/login");
-      });
-    }
-  });
+//       configElement.innerHTML = "CONFIGURAÇÕES";
+//       configElement.href = "/configuracoes";
+//       logoutButton.innerHTML = "SAIR";
+//       logoutButton.removeAttribute("href");
+//       logoutButton.addEventListener("click", () => {
+//         localStorage.removeItem("token");
+//         fetch("https://debugrace-backend.onrender.com/deslog", {
+//           method: "POST",
+//           headers: { "Content-type": "application/json" },
+//           body: JSON.stringify({ email: usuarioObject.email }),
+//         }).then((res) => res.json());
+//         localStorage.removeItem("usuario");
+//         window.location.assign("https://debugrace-30568.web.app/login");
+//       });
+//     }
+//   });
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
